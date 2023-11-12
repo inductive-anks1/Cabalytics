b7c2b3631cf87c1529e750f586aeea08414e40d9
Ankit Kumar <102726316+inductive-anks1@users.noreply.github.com>
2023-11-12 12:29:08 +0530

    Add files via upload
    Streamlit Initial Framework

--- app.py
+++ app.py
@@ -16,3 +16,23 @@ pick_up = st.selectbox('Pick Up', df['Pick_Up'].unique())
 
 #Destination
 destination = st.selectbox('Destination', df['Destination'].unique())
+
+#Current Day
+current_day = st.selectbox('Day', df['Current Day'].unique())
+
+#Hour
+unique_hours = sorted(df['Current_Hour'].unique())
+current_hour = st.selectbox('Hour', unique_hours)
+
+#Minute
+current_min = st.number_input('Current Minute')
+
+#Month
+current_month = st.selectbox('Month', ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])
+
+#Year
+current_year = st.number_input('Current Year')
+
+
+if st.button('Predict Price'):
+    pass
